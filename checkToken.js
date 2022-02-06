acb05223b51824990021d1e69e674dc0e3f85e21
MatheusRoichman <roichmankill@gmail.com>
2022-02-06 08:49:42 -0300

    Logging e tratamento de erros no middleware de checagem do access token

--- checkToken.js
+++ checkToken.js
@@ -1,9 +1,13 @@
 const jwt = require('jsonwebtoken');
+const { log } = require('./logger');
+const serverErrorMessage = require('./serverErrorMessage');
 
 const checkToken = (req, res, next) => {
   const { accessToken } = req.cookies;
 
   if (!accessToken) {
+    log('Access token check', 'Unsuccessful', 'Access denied');
+
     return res.status(401).json({
       message: 'Acesso negado!'
     });
@@ -16,9 +20,20 @@ const checkToken = (req, res, next) => {
 
     return next();
   } catch (error) {
-    return res.status(401).json({
-      error: 'Token inválido!'
-    })
+    if(error instanceof jwt.TokenExpiredError) {
+      log('Access token check', 'Unsuccessful', 'Expired token');
+
+      return res.status(401).json({
+        error: 'Token expirado!'
+      })
+    } else {
+      log('Access token check', 'Internal server error', error.message);
+
+      return res.status(500).json({
+        error
+      })
+    }
+    
   }
 };
 
