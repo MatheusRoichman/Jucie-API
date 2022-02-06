0e53a8b152f6cc7fecccf411404227de23e06591
MatheusRoichman <roichmankill@gmail.com>
2022-02-06 08:51:48 -0300

    Logging de todas as requisições e respostas

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
-const moment = require('moment');
-const morganBody = require('morgan-body');
+
 const cookieParser = require('cookie-parser');
 const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const { customLogger } = require('./logger');
+const { createLogger, transports, format } = require('winston');
+const moment = require('moment');
+const morganBody = require('morgan-body');
 
 require("dotenv").config();
 
@@ -22,27 +23,55 @@ app.use(
 
 app.use(express.json());
 
-const log = fs.createWriteStream(
-  path.join(__dirname, "./logs", `express${moment().format('YYYY-MM-DD')}.log`), { flags: "a" }
-);
+const logLevels = {
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+};
+
+const logger = createLogger({
+  levels: logLevels,
+  transports: [
+    new transports.File({
+      filename: `./logs/express-${moment().format('YYYY-MM-DD')}.log`
+    })
+  ],
+  format: format.combine(format.timestamp(), format.simple())
+});
+
+const loggerStream = {
+  write: message => {
+    logger.info(message);
+  },
+};
+
+const morganBodyConfig = {
+  noColors: true,
+  logAllReqHeader: true,
+  logRequestId: true,
+  logAllResHeader: true
+};
 
 morganBody(app, {
-  noColors: true,
-  stream: log,
+  ...morganBodyConfig,
+  stream: loggerStream
 });
+
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 const mongodbUrl = {
   dev: 'mongodb://0.0.0.0:27017/jucie-dev',
   prod: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ykbsq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 }
 
-app.use('/produtos', productRoutes);
-app.use('/auth', authRoutes);
-
 mongoose.connect(mongodbUrl.dev).then(() => {
-  console.log('Connected to MongoDB');
+  customLogger.info('Connected to MongoDB');
 }).catch(err => {
-  console.log('Error on connecting to MongoDB: ' + err);
+  customLogger.info('Error on connecting to MongoDB: ' + err);
 });
 
 app.listen(3000);
